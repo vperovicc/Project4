1a925c1c990e41bd295f57ef776c7f7011521fce
perovicc <vukasinperovic10@gmail.com>
2024-01-02 13:02:32 +0100

    odradjena klasa Log sa svim potrebnim interfacima i omoguceno logovanje poruka, warninga, i errora

---
+++ Log/Implementation/LogFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log.Implementation
+{
+    public class LogFile
+    {
+        private string fileName = "log.txt";
+
+        public LogFile(string fn = "log.txt")
+        {
+            if(fileName == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(fileName.Trim().Length == 0 )
+            {
+                throw new ArgumentException();
+            }
+
+            fileName = fn;
+        }
+
+        public void LogInFile(string mess)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName,true))
+            {
+                sw.WriteLine(mess);
+            }
+        }
+    }
+}
---
+++ Log/Interface/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log.Interface
+{
+    public interface ILog
+    {
+        void NewMessage();
+        void NewWarning();
+        void NewError();
+    }
+}
