da88813804ab63576cbc37a5b84d5ffc3e6015bf
StefanDangubic <stefan.dangubic.ns@gmail.com>
2024-01-08 17:37:51 +0100

    Implementacija dugmeta za dodavanje uredjaja i izmene u MainWindow.xaml

--- SmartThermoregulator/MainInterface/MainWindow.xaml.cs
+++ SmartThermoregulator/MainInterface/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Heater;
 using Common.Implementation;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.Remoting;
 
 namespace MainInterface
 {
@@ -24,40 +27,66 @@ namespace MainInterface
     {
         Regulator reg = null;
         DeviceCollection devices = new DeviceCollection();
-        
+        int[] port = new int[100];
+        int instance = 0;
+        List<Device.Device> display = new List<Device.Device>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 100; i++)
+                port[i] = 8500 + i;
         }
 
-        private void baseMode()
+        private void btnDodaj_Click(object sender, RoutedEventArgs e)
         {
-            reg = new Regulator();
+            try
+            {
+                
+                string dir = Environment.CurrentDirectory;
+                dir = Directory.GetParent(dir).Parent.Parent.FullName;
 
-            reg.Uredjaji = devices.GetDevices();
-            reg.ZeljenaDnevnaTemperatura = Double.Parse(tbTempDnevna.Text);
-            reg.PocetakDnevniRezim = Int32.Parse(cbOdDnevni.Text);
-            reg.KrajDnevniRezim = Int32.Parse(cbDoDnevni.Text);
+                Process process = new Process();
 
-            btnDodaj.IsEnabled = false;
+                process.StartInfo.FileName = dir + "\\bin\\Debug\\Device.exe";
+                process.StartInfo.Arguments = port[instance].ToString();
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+
+                process.Start();
+
+                string stringConn = "tcp://localhost:" + port[instance].ToString() + "/Device";
+                Device.Device mr = RemotingServices.Connect(typeof(Device.Device), stringConn) as Device.Device;
+
+                if (devices.AddDevice(mr))
+                {
+                    MessageBox.Show("Uređaj je uspešno dodat!", "Informacija", MessageBoxButton.OK, MessageBoxImage.Information);
+                    instance++; 
+
+                    
+                    var tmp = new Device.Device(mr.DeviceID, mr.Temperature);
+                    display.Add(tmp);
+                }
+                else
+                {
+                    MessageBox.Show("Nije moguće dodati novi uređaj!\nPrekoračili ste granicu za dodavanje uredjaja!", "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Upozorenje", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+
         }
 
-        private void checking(int pd, int kd)
+        private void btnSacuvaj_Click(object sender, RoutedEventArgs e)
         {
-            cbOdDnevni.IsEnabled = false;
-            cbDoDnevni.IsEnabled = false;
-            btnSacuvaj.IsEnabled = false;
-            tbTempDnevna.IsEnabled = false;
-            btnPocetak.IsEnabled = true;
-            tbTempNocna.IsEnabled = true;
 
-            cbOdNocni.Items.Clear();
-            cbDoNocni.Items.Clear();
+        }
 
-            cbOdNocni.Items.Add(kd);
-            cbDoNocni.Items.Add(pd);
-            cbOdNocni.SelectedIndex = 0;
-            cbDoNocni.SelectedIndex = 0;
+        private void btnPocetak_Click(object sender, RoutedEventArgs e)
+        {
 
         }
 
@@ -65,5 +94,45 @@ namespace MainInterface
         {
             Close();
         }
+
+
+
+
+
+
+
+          private void baseMode()
+          {
+              reg = new Regulator();
+
+              reg.Uredjaji = devices.GetDevices();
+              reg.ZeljenaDnevnaTemperatura = Double.Parse(TempDnevna.Text);
+              reg.PocetakDnevniRezim = Int32.Parse(odDnevni.Text);
+              reg.KrajDnevniRezim = Int32.Parse(doDnevni.Text);
+
+              btnDodaj.IsEnabled = false;
+          }
+
+          private void checking(int pd, int kd)
+          {
+              odDnevni.IsEnabled = false;
+              doDnevni.IsEnabled = false;
+              btnSacuvaj.IsEnabled = false;
+              TempDnevna.IsEnabled = false;
+              btnPocetak.IsEnabled = true;
+              TempNocna.IsEnabled = true;
+
+              odNocni.Items.Clear();
+              doNocni.Items.Clear();
+
+              odNocni.Items.Add(kd);
+              doNocni.Items.Add(pd);
+              odNocni.SelectedIndex = 0;
+              doNocni.SelectedIndex = 0;
+
+          }
+
+         
+        
     }
 }
